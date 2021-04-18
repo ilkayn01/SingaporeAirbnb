50e42b986d0cbac9dc3e7c83523e7cda95bcc3f2
elloise <49631121+KalllN@users.noreply.github.com>
2021-04-18 14:01:58 +0530

    libraries

---
+++ packages/libraries.R
@@ -0,0 +1,32 @@
+#Importing libraries
+if(!require(readr)) install.packages("readr", repos = "http://cran.us.r-project.org")
+
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+
+if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+
+if(!require(DataExplorer)) install.packages("DataExplorer", repos = "http://cran.us.r-project.org")
+
+if(!require(corrplot)) install.packages("corrplot", repos = "http://cran.us.r-project.org")
+
+if(!require(leaps)) install.packages("leaps", repos = "http://cran.us.r-project.org")
+
+if(!require(glmnet)) install.packages("glmnet", repos = "http://cran.us.r-project.org")
+
+if(!require(randomForest)) install.packages("randomForest", repos = "http://cran.us.r-project.org")
+
+library(readr)
+
+library(tidyverse)
+
+library(DataExplorer)
+
+library(ggplot2)
+
+library(corrplot)
+
+library(leaps)
+
+library(glmnet)
+
+library(randomForest)
