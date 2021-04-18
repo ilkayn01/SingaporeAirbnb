e67e0b12f10b03d426e692c615d81296920b50ae
elloise <49631121+KalllN@users.noreply.github.com>
2021-04-18 14:20:36 +0530

    Update linear_reg.R

--- regression/linear_reg.R
+++ regression/linear_reg.R
@@ -1,16 +1,18 @@
 #Linear Regression Model without reviews
 
 #MSE - Mean of squared errors
-
 #R Squared - goodness of fit measure for LR models
-
 #Adjusted R squared helps us determine an optimal model better than just R squared
 
 #First, we make a model without reviews to check how a model with and without reviews would affect our prediction score.
 
-lr_model <- lm(price ~ neighbourhood_group + latitude + longitude + 
-
-room_type + minimum_nights + availability_365, data = train_set)
+lr_model <- lm(price ~ neighbourhood_group + 
+               latitude + 
+               longitude + 
+room_type + 
+               minimum_nights + 
+               availability_365, 
+               data = train_set)
 
 summary_lr_model <- summary(lr_model)
 
@@ -28,7 +30,8 @@ lr_model_adj_rsquared <- summary_lr_model$adj.r.squared
 
 model_results <- tibble()
 
-model_results <- tibble(method = "Linear Regression Model", MSE = lr_model_mse, 
+model_results <- tibble(method = "Linear Regression Model", 
+                        MSE = lr_model_mse, 
                         R_squared = lr_model_rsquared, 
                         Adjusted_R_squared = lr_model_adj_rsquared)
 
